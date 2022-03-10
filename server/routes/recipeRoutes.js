70fa56d89782675a2c6d10a604d9103979c282e0
manirajantu <94602111+manirajantu@users.noreply.github.com>
2022-03-10 15:03:15 +0800

    4

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -11,19 +11,33 @@ const recipeController = new RecipeController();
 
 router.route("/recipe/showIngredient").get((request, response) => {
   recipeController.showIngredient();
-  response.send("You have called the showIngredient route!");
+  then(showIngredient => {
+    res.status(200).json(showIngredient)
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'unable to retrieve data'})
+  })
 });
 
 router.route("/recipe/showRecipe").get((request, response) => {
   recipeController.showRecipe();
-  response.send("You have called the showRecipe route!");
+  then(showRecipe => {
+    res.status(200).json(showRecipe)
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'unable to retrieve data'})
+  })
 });
 
 router.route("/recipe/showUserRecipe").get((request, response) => {
   recipeController.showUserRecipe();
-  response.send("You have called the showUserRecipe route!");
+  then(showUserRecipe => {
+    res.status(200).json(showUserRecipe)
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'unable to retrieve data'})
+  })
 });
-
 router
   .route("/add")
   .get((request, response) => {
